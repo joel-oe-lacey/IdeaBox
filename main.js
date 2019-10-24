c6e26fe8a820d1c6be0a452ed8de7a515322a437
Joel Lacey <joellaceybusiness@gmail.com>
2019-10-24 09:25:43 -0600

    Add star toggle and pseudocode notes on star function

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ var bodyInput = document.querySelector('.form-textarea-body');
 var submit = document.querySelector('.form-button-save');
 var cardSection = document.querySelector('.section-cards');
 
-//add event listner for click
 submit.addEventListener('click', function() {
     var titleValue = titleInput.value;
     var bodyValue = bodyInput.value;
@@ -18,5 +17,13 @@ submit.addEventListener('click', function() {
 cardSection.addEventListener('click', function() {
   if (event.target.id === 'delete') {
     event.target.parentNode.parentNode.parentNode.remove();
+  } else if (event.target.id === 'star') {
+    console.log(event);
+    var src = event.target.src;
+    if (src.includes('active')) {
+      event.target.src = "assets/star.svg";
+    } else {
+      event.target.src = "assets/star-active.svg";
+    }
   }
 });
--- pseudocode-notes.md
+++ pseudocode-notes.md
@@ -10,3 +10,11 @@ When user clicks submit
     2.2) assign title and body based on input variables
   3) call saveToStorage method
     3.1) injects inner html into cards-section
+
+
+For star functionality
+//need to store an array of objects of all cards
+//need to add card object to array otherwise it's lost after click
+//also need to store ID value somewhere in the HTML of the card so it can be referenced back later
+//to change star value need to change image based on star status
+//but also need to go into total card array list, reference ID value of card, and set this.starred to true, do inverse if unchecking.
